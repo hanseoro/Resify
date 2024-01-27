fc1f6dd631a44e5f18b74e0f60566b96e0567c61
kylebtran <146040347+kylebtran@users.noreply.github.com>
2024-01-27 15:16:18 -0800

    Tinkered front page and fonts

--- resify/app/page.tsx
+++ resify/app/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
         margin: '0 auto'
       }}>
         <Navbar />
+        <Hero />
         <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#28145C_100%)]" />
       </div>
     </>
