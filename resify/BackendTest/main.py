b7d5750616838db252d8f7bb9299bba25033c832
Han Seo Ro <hsro@uci.edu>
2024-01-27 17:29:04 -0800

    added user signup

--- resify/BackendTest/main.py
+++ resify/BackendTest/main.py
@@ -10,13 +10,20 @@ app = FastAPI()
 
 @app.post("/verified_home")
 def verified_home(item: dict):
+    #data = {address1:"",address2:"",state:"", zip5:"",email:""}
     if(item["response"]):
         data = {"address1":item["a1"],"address2":item["a2"],"state":item["admarea"], "zip5":item["postal"],email:""}
         add_property(data)
         raise NotImplemented
     print(item)
 
+
 @app.post("/signup_user_data")
 def signup_user_data(info: dict):
-
+    #data = {first_name:"",last_name:"", email:"", phone_number:"", password:"", properties: []}
+    data = []
+    add_user(data)
+    
+    
+    
     raise NotImplementedError
