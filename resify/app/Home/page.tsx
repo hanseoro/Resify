452d5cda253d7386f4e9451b8549f0358b809fdd
Johnny Do <dojohnny847@gmail.com>
2024-01-28 09:19:11 -0800

    Changed pages routing and added alert dialogs and functionality

--- resify/app/Home/page.tsx
+++ resify/app/Home/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import NavbarHome from "@/components/NavbarHome";
+<<<<<<< Updated upstream
 import { auth, currentUser, useUser } from "@clerk/nextjs";
 import axios from "axios";
 import Hero from "@/components/Hero";
@@ -7,21 +8,46 @@ import { EmailAddress } from "@clerk/nextjs/server";
 import { Alert } from "reactstrap";
 import { useState } from "react";
 
+=======
+import cn from "@/components/Navbar.module.css";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { SignOutButton } from "@clerk/nextjs";
+>>>>>>> Stashed changes
 // import '../styles/fonts.css';
 
 export default function Home() {
   const CallAlert = () => {
     alert("Your id is");
   };
+<<<<<<< Updated upstream
+=======
+  /* const user = await currentUser();
+>>>>>>> Stashed changes
 
   /* const data = {
     first_name: user?.firstName,
     last_name: user?.lastName,
     email: user?.emailAddresses[0].emailAddress,
     phone_numer: user?.phoneNumbers[0],
+<<<<<<< Updated upstream
   };
   await axios.post("http://127.0.0.1:8000/signup_user_data", data); å
   console.log(user); */
+=======
+  }; */
+  /* await axios.post("http://127.0.0.1:8000/signup_user_data", data); */
+
+>>>>>>> Stashed changes
   return (
     <>
       <div
@@ -61,6 +87,7 @@ export default function Home() {
                   maxWidth: "300px",
                 }}
               >
+<<<<<<< Updated upstream
                 <h3 style={{ margin: "0 0 20px 0", textAlign: "center" }}>
                   Card Title 1
                 </h3>
@@ -88,6 +115,54 @@ export default function Home() {
                 >
                   Generate Certificate ID
                 </a>
+=======
+                <h3
+                  style={{ margin: "0 0 20px 0", textAlign: "center" }}
+                  className={"text-secondary " + cn.videosemibold}
+                >
+                  Generate Code
+                </h3>
+                <p
+                  style={{ textAlign: "left" }}
+                  className={"text-secondary " + cn.videonormal}
+                >
+                  Properties that have previously been verified by you will
+                  appear here. A unique code will be generated that you may
+                  share.
+                </p>
+                <AlertDialog>
+                  <AlertDialogTrigger asChild>
+                    <button
+                      style={{
+                        marginTop: "200px",
+                        padding: "10px 20px",
+                        borderRadius: "1px",
+                        border: "none",
+                        background: "white",
+                        color: "#000",
+                        display: "block",
+                        margin: "auto",
+                      }}
+                      className={"text-secondary " + cn.videomedium}
+                    >
+                      GET CODE
+                    </button>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>
+                        Here is your certificate code:
+                      </AlertDialogTitle>
+                      <AlertDialogDescription>
+                        Jx9R4m2NlFb78PqE3iTk
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogAction>Please Sign Out</AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
+>>>>>>> Stashed changes
               </div>
 
               {/* Card 2 */}
@@ -101,6 +176,7 @@ export default function Home() {
                   maxWidth: "300px",
                 }}
               >
+<<<<<<< Updated upstream
                 <h3 style={{ margin: "0 0 20px 0", textAlign: "center" }}>
                   Card Title 2
                 </h3>
@@ -126,6 +202,37 @@ export default function Home() {
                   }}
                 >
                   Verify Property
+=======
+                <h3
+                  style={{ margin: "0 0 20px 0", textAlign: "center" }}
+                  className={"text-secondary " + cn.videosemibold}
+                >
+                  Property Verification
+                </h3>
+                <p
+                  style={{ textAlign: "left" }}
+                  className={"text-secondary " + cn.videonormal}
+                >
+                  Register owned property by verifying identity. Estates will be
+                  held in account privately until code is generated.
+                </p>
+                <a href="/Verify">
+                  <button
+                    style={{
+                      marginTop: "200px",
+                      padding: "10px 20px",
+                      borderRadius: "1px",
+                      border: "1px solid #fff",
+                      background: "black",
+                      color: "#fff",
+                      display: "block",
+                      margin: "auto",
+                    }}
+                    className={"text-secondary " + cn.videomedium}
+                  >
+                    VERIFY
+                  </button>
+>>>>>>> Stashed changes
                 </a>
               </div>
             </div>
