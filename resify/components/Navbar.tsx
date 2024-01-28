d95818f45ef475c4b0e302dd63f61a90a3c9b3bf
kylebtran <146040347+kylebtran@users.noreply.github.com>
2024-01-27 16:24:46 -0800

    Tweaked

--- resify/components/Navbar.tsx
+++ resify/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar: React.FC = () => {
             <div className="max-w-6xl mx-auto">
                 <div className="flex justify-between">
                     <div className="flex space-x-4"> 
-                        <a href="#" className={"flex items-center py-5 text-secondary " + cn.videobold}>Resify</a>
+                        <a href="#" className={"flex items-center py-5 px-2 text-secondary " + cn.videobold}>Resify</a>
                         <div className="hidden md:flex items-center space-x-1">
                             <a href="#" className={"py-5 px-4 text-secondary hover:text-opacity-80 " + cn.videonormal}>Collective</a>
                             <a href="#" className={"py-5 px-4 text-secondary hover:text-opacity-80 " + cn.videonormal}>Services</a>
